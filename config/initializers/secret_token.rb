0832d6769fd4419fbf18df3222bd9b36656fb486
xiongbo <xiongbo027@gmail.com>
2013-09-30 10:16:26 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HeatmapDemo::Application.config.secret_key_base = 'bee619fb07c07882bb7e7bd649b0d2ead6455206e3a1b5e0cffb78d6ae0b03f784269996e08a2e9355b2964f91c9d7c2af3c3ef34a8f5d7358676583fb8035e2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HeatmapDemo::Application.config.session_store :cookie_store, key: '_heatmap-demo_session'
