44f6feced79415f2932fe5739a838768522d7392
xiongbo <xiongbo027@gmail.com>
2013-11-21 16:47:19 +0800

    add api support for cros

--- app/controllers/heats_controller.rb
+++ app/controllers/heats_controller.rb
@@ -1,4 +1,7 @@
 class HeatsController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+  before_filter :cors_preflight_check
+  after_filter :cors_set_access_control_headers
 
   def create
     site = Site.find(params[:site_id])
@@ -12,6 +15,16 @@ class HeatsController < ApplicationController
   end 
 
   private
+  def cors_preflight_check
+    headers['Access-Control-Allow-Origin'] = "*"
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+  end
+
+  def cors_set_access_control_headers 
+    headers['Access-Control-Allow-Origin'] = "*"
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+  end
+
   def heat_params
     params.require(:heat).permit(:xpoint, :ypoint, :screen_ratio)
   end 
